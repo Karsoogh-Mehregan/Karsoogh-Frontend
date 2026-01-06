7b479981ea3039ab8b2fbbe1ce17f14e9232f1aa
AliAlmasiZ <ali1385almasi@gmail.com>
2026-01-06 15:40:42 +0330

    fix: coverage and XOCanvas

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,30 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+const mockContext = {
+  clearRect: vi.fn(),
+  fillRect: vi.fn(),
+  beginPath: vi.fn(),
+  arc: vi.fn(),
+  fill: vi.fn(),
+  stroke: vi.fn(),
+  moveTo: vi.fn(),
+  lineTo: vi.fn(),
+  closePath: vi.fn(),
+  save: vi.fn(),
+  restore: vi.fn(),
+  translate: vi.fn(),
+  rotate: vi.fn(),
+  scale: vi.fn(),
+  fillText: vi.fn(),
+  // ویژگی‌هایی (Properties) که در کامپوننت مقداردهی می‌کنید
+  font: '',
+  fillStyle: '',
+  textAlign: 'start',
+  textBaseline: 'alphabetic',
+};
+
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  value: vi.fn(() => mockContext),
+  writable: true,
+});
